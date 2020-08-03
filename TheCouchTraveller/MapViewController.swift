0d1a7b114fc0949973dd5faa0581fefc1eb224fa
Henrique Abreu <abreu-semperfi@icloud.com>
2020-08-03 11:12:29 +0100

    added: MapViewController can now handle the insertion of pins via longPressGestureRecognizer
    inserted the basic functions in the vc so we can start handling the pin creation, next step would be starting the CoreData conversion

---
+++ TheCouchTraveller/MapViewController.swift
@@ -0,0 +1,77 @@
+//
+//  MapViewController.swift
+//  TheCouchTraveller
+//
+//  Created by Henrique Abreu on 03/08/2020.
+//  Copyright © 2020 Henrique Abreu. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet var longPressRec: UILongPressGestureRecognizer!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var annotations = [MKPointAnnotation]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+    
+    }
+
+
+// MARK: - MKMapViewDelegate
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    
+        let reuseId = "pin"
+    
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+    
+        return pinView
+    }
+    
+
+    @IBAction func handleLongPress(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == .began {
+            print("BEGAN")
+        } else if sender.state == .ended {
+            let coord = mapView.convert(sender.location(in: mapView), toCoordinateFrom: mapView)
+            saveGeoCoordination(from: coord)
+
+        }
+    }
+    
+    /// get Geo position and store value Pin viewContext
+    func saveGeoCoordination(from coordinate: CLLocationCoordinate2D) {
+        let geoPos = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        let annotation = MKPointAnnotation()
+        CLGeocoder().reverseGeocodeLocation(geoPos) { (placemarks, error) in
+            guard let placemark = placemarks?.first else { return }
+            annotation.title = placemark.name ?? "Not Known"
+            annotation.subtitle = placemark.country
+            annotation.coordinate = coordinate
+            DispatchQueue.main.async {
+                self.mapView.addAnnotation(annotation)
+
+            }
+        }
+    }
+}
+
+
+
